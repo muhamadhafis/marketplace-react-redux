63f0fac5a12dabdf9886c732b972bc8739dc2edb
Hafis <hafisgtngm199@gmail.com>
2025-06-22 10:59:41 +0700

    Apply react-hook-form for menagments form

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -10,81 +10,88 @@ import { Label } from "@/components/ui/label";
 import { Checkbox } from "@/components/ui/checkbox";
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormItem,
+  FormMessage,
+  FormLabel,
+  FormField,
+  FormControl,
+} from "@/components/ui/form";
 
 const LoginPage = () => {
-  const [inputUsername, setInputUsername] = useState("");
-  const [inputPassword, setInputPassword] = useState("");
+  const form = useForm({
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  });
   const [isChecked, setIsChecked] = useState(false);
-  const [inputUsernameMessage, setInputUsernameMessage] = useState("");
-  const [inputPasswordMessage, setInputPasswordMessage] = useState("");
-  const handleLogin = () => {
-    alert(inputUsername + inputPassword);
+  const handleLogin = (values) => {
+    alert(values.username + values.password);
   };
 
   return (
     <main className="flex justify-center items-center flex-col gap-2 h-[80vh]">
-      <Card className={"w-full max-w-lg"}>
-        <CardHeader>
-          <CardTitle className={"text-2xl font-bold tracking-tighter"}>
-            Welcome back
-          </CardTitle>
-        </CardHeader>
-        <CardContent className={"flex flex-col gap-4"}>
-          <div className="flex flex-col gap-2">
-            <Label htmlFor="username">Username</Label>
-            <Input
-              onChange={(e) => {
-                if (e.target.value.length < 3) {
-                  setInputUsernameMessage(
-                    "Username must be at least 3 characters long"
-                  );
-                } else {
-                  setInputUsernameMessage("");
-                }
-                setInputUsername(e.target.value);
-              }}
-              id="username"
-            />
-            <p className="text-red-400 text-xs">{inputUsernameMessage}</p>
-          </div>
-          <div className="flex flex-col gap-2">
-            <Label htmlFor="password">Password</Label>
-            <Input
-              onChange={(e) => {
-                if (e.target.value.length < 8) {
-                  setInputPasswordMessage(
-                    "Password must be at least 8 characters long"
-                  );
-                } else {
-                  setInputPasswordMessage("");
-                }
-                setInputPassword(e.target.value);
-              }}
-              type={isChecked ? "text" : "password"}
-              id="password"
-            />
-            <p className="text-red-400 text-xs">{inputPasswordMessage}</p>
-          </div>
-          <div className="flex items-center gap-2">
-            <Checkbox
-              id="show-password"
-              onCheckedChange={(checked) => setIsChecked(checked)}
-            />
-            <Label htmlFor="show-password">Show Password</Label>
-          </div>
-        </CardContent>
-        <CardFooter>
-          <div className="flex flex-col w-full gap-4">
-            <Button
-              disabled={inputUsername.length < 3 || inputPassword.length < 8}
-              onClick={handleLogin}
-            >
-              Log in
-            </Button>
-            <Button variant="link">Sign up as guest</Button>
-          </div>
-        </CardFooter>
-      </Card>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(handleLogin)}
+          className={"w-full max-w-lg"}
+        >
+          <Card>
+            <CardHeader>
+              <CardTitle className={"text-2xl font-bold tracking-tighter"}>
+                Welcome back
+              </CardTitle>
+            </CardHeader>
+            <CardContent className={"flex flex-col gap-4"}>
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Username</FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        type={isChecked ? "text" : "password"}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <div className="flex items-center gap-2">
+                <Checkbox
+                  id="show-password"
+                  onCheckedChange={(checked) => setIsChecked(checked)}
+                />
+                <Label htmlFor="show-password">Show Password</Label>
+              </div>
+            </CardContent>
+            <CardFooter>
+              <div className="flex flex-col w-full gap-4">
+                <Button type="submit">Log in</Button>
+                <Button variant="link">Sign up as guest</Button>
+              </div>
+            </CardFooter>
+          </Card>
+        </form>
+      </Form>
     </main>
   );
 };
