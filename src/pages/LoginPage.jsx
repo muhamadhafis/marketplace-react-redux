c8ea34bc006493ace3ae89454af9841bc5433ffd
Hafis <hafisgtngm199@gmail.com>
2025-06-22 11:33:10 +0700

    Validate form using zod

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -19,6 +19,16 @@ import {
   FormField,
   FormControl,
 } from "@/components/ui/form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const loginFormSchema = z.object({
+  username: z
+    .string()
+    .min(3, "Username must be at least 3 characters")
+    .max(16, "Username less than 16 characters"),
+  password: z.string().min(8, "Password must be at least 8 characters"),
+});
 
 const LoginPage = () => {
   const form = useForm({
@@ -26,6 +36,8 @@ const LoginPage = () => {
       username: "",
       password: "",
     },
+    resolver: zodResolver(loginFormSchema),
+    reValidateMode: "onSubmit",
   });
   const [isChecked, setIsChecked] = useState(false);
   const handleLogin = (values) => {
