79f82e4273f8caed62d08558175af517ba5889f4
Hafis <hafisgtngm199@gmail.com>
2025-06-24 11:19:23 +0700

    Add pagination at admin dashboard

--- src/pages/admin/ProductManagmentPage.jsx
+++ src/pages/admin/ProductManagmentPage.jsx
@@ -1,6 +1,11 @@
 import { AdminLayout } from "@/components/Layout/AdminLayout";
 import { Button } from "@/components/ui/button";
 import {
+  Pagination,
+  PaginationContent,
+  PaginationItem,
+} from "@/components/ui/pagination";
+import {
   Table,
   TableHeader,
   TableHead,
@@ -9,27 +14,56 @@ import {
   TableCell,
 } from "@/components/ui/table";
 import { axiosInstance } from "@/lib/axios";
-import { Ellipsis } from "lucide-react";
+import { ChevronLeft, ChevronRight, Ellipsis } from "lucide-react";
 import { useEffect, useState } from "react";
 import { IoAdd } from "react-icons/io5";
+import { useSearchParams } from "react-router-dom";
 
 const ProductManagmentPage = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+
   const [products, setProducts] = useState([]);
+  const [hasNextPage, setHasNextPage] = useState(true);
+
+  const handleNextPage = () => {
+    searchParams.set("page", Number(searchParams.get("page")) + 1);
+    setSearchParams(searchParams);
+  };
+
+  const handlePreviousPage = () => {
+    searchParams.set("page", Number(searchParams.get("page")) - 1);
+    setSearchParams(searchParams);
+  };
+
   const fetchProducts = async () => {
     try {
       const response = await axiosInstance.get("/products", {
         params: {
-          _limit: 5,
+          _per_page: 5,
+          _page: Number(searchParams.get("page")),
         },
       });
-      setProducts(response.data);
+      console.log(response.data);
+      setHasNextPage(Boolean(response.data.next));
+      setProducts(response.data.data);
     } catch (error) {
       console.log(error);
     }
   };
+
   useEffect(() => {
-    fetchProducts(), [];
-  });
+    if (searchParams.get("page")) {
+      fetchProducts();
+    }
+  }, [searchParams.get("page")]);
+
+  useEffect(() => {
+    if (!searchParams.get("page")) {
+      searchParams.set("page", 1);
+      setSearchParams(searchParams);
+    }
+  }, []);
+
   return (
     <div>
       <AdminLayout
@@ -72,6 +106,33 @@ const ProductManagmentPage = () => {
             })}
           </TableBody>
         </Table>
+        <Pagination className={"mt-6"}>
+          <PaginationContent>
+            <PaginationItem>
+              <Button
+                disabled={searchParams.get("page") === "1"}
+                variant={"ghost"}
+                onClick={handlePreviousPage}
+              >
+                <ChevronLeft className="w-6 h-6" />
+                Previous
+              </Button>
+            </PaginationItem>
+            <PaginationItem className={"mx-4 font-semibold"}>
+              Page {searchParams.get("page")}
+            </PaginationItem>
+            <PaginationItem>
+              <Button
+                disabled={!hasNextPage}
+                variant={"ghost"}
+                onClick={handleNextPage}
+              >
+                Next
+                <ChevronRight className="w-6 h-6" />
+              </Button>
+            </PaginationItem>
+          </PaginationContent>
+        </Pagination>
       </AdminLayout>
     </div>
   );
